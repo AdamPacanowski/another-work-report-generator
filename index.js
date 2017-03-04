513b5e9ec508865691ae7c6841ee44ab71d81155
Adam Pacanowski <pacibum@gmail.com>
2017-03-04 21:57:34 +0100

    Add month setting

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const version = require('./package.json').version;
 const prog = require('caporal');
 
-prog.version(version);
+const settings = {
+
+};
+
+prog
+    .version(version)
+    .option('--month <month>', 'Set report month', /^(0?[1-9]|1[012])$/)
+    .action((args, options, logger) => {
+      settings.month = args[1];
+      logger.debug(`Month = ${ settings.month }`);
+    });
 
 
 
 prog.parse(process.argv);
+
+
+
+
+console.log(`Month`);
