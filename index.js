56099f0b8f1433397345c51d03db8b3711751f54
Adam Pacanowski <pacibum@gmail.com>
2017-03-04 22:28:26 +0100

    Add other options

--- index.js
+++ index.js
@@ -7,10 +7,40 @@ const settings = {
 
 prog
     .version(version)
-    .option('--month <month>', 'Set report month', /^(0?[1-9]|1[012])$/)
+    .option('--path <path>', 'repositories root path')
     .action((args, options, logger) => {
-      settings.month = args[1];
+      settings.path = options.path;
+      logger.debug(`Path = ${ settings.path }`);
+    })
+    .option('--month <month>', 'report month', /^(0?[1-9]|1[012])$/)
+    .action((args, options, logger) => {
+      settings.month = options.month;
       logger.debug(`Month = ${ settings.month }`);
+    })
+    .option('--year <year>', 'report year', prog.INT)
+    .action((args, options, logger) => {
+      settings.year = options.year;
+      logger.debug(`Year = ${ settings.year }`);
+    })
+    .option('--author <author>', 'report author name')
+    .action((args, options, logger) => {
+      settings.author = options.author;
+      logger.debug(`Author = ${ settings.author }`);
+    })
+    .option('--max-hours-per-day <hours>', 'number of max hours per day')
+    .action((args, options, logger) => {
+      settings.maxHoursPerDay = options.hours;
+      logger.debug(`Max hours per day = ${ settings.maxHoursPerDay }`);
+    })
+    .option('--min-commit-time <hours>', 'number of min commit time in hours' )
+    .action((args, options, logger) => {
+      settings.minCommitTime = options.hours;
+      logger.debug(`Min commit time = ${ settings.minCommitTime }`);
+    })
+    .option('--graduation <hours>', 'smallest time unit in hours')
+    .action((args, options, logger) => {
+      settings.graduation = options.graduation;
+      logger.debug(`Graduation = ${ settings.graduation }`)
     });
 
 
