06b71a6b96e972ff7624ede96b525ae5d0f9e62d
Adam Pacanowski <pacibum@gmail.com>
2017-03-21 21:59:17 +0100

    Split commitsGetter file

--- src/commitsGetter.js
+++ src/commitsGetter.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
 
-module.exports = function(settings) {
+function findRepositories(settings) {
   return fs
     .readdirSync(settings.path)
     .filter((folderName) => fs.lstatSync(path.join(settings.path, folderName)).isDirectory())
@@ -15,13 +15,23 @@ module.exports = function(settings) {
       }
 
       return true;
-    })
-    .map((gitFolder) => ({
-      gitFolder,
-      commits: execSync(
-        `git --git-dir ${ gitFolder } log ` +
-        `--after="${ settings.startTime.toISOString() }" --before="${ settings.endTime.toISOString() }" --author="${ settings.author }" ` +
-        '--branches --no-merges  --pretty=format:"%H;%h;%at;%s" --shortstat'
-      ).toString().split('\n\n').filter((commit) => !!commit.length)
-    }));
+    });
+}
+
+function getRepositoriesCommits(settings, gitFolders) {
+  return gitFolders.map((gitFolder) => ({
+    gitFolder,
+    commits: execSync(
+      `git --git-dir ${ gitFolder } log ` +
+      `--after="${ settings.startTime.toISOString() }" --before="${ settings.endTime.toISOString() }" --author="${ settings.author }" ` +
+      '--branches --no-merges  --pretty=format:"%H;%h;%at;%s" --shortstat'
+    ).toString().split('\n\n').filter((commit) => !!commit.length)
+  }));
+}
+
+module.exports = function(settings) {
+  const gitFolders = findRepositories(settings);
+  const commits = getRepositoriesCommits(settings, gitFolders);
+
+  return commits;
 };
